b08d8e54c27c6e58641671309841a173efe531ca
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-09-24 20:34:05 +0300

    Add Subscriber model with JSON serialization

--- lib/models/subscriber_model.dart
+++ lib/models/subscriber_model.dart
@@ -1,1 +1,43 @@
+import 'dart:convert';
+
+class Subscriber {
+  final String deviceId;
+  final String fullName;
+  final bool isPaid;
+  final String expiryDate;
+  final String notes;
+
+  Subscriber({
+    required this.deviceId,
+    required this.fullName,
+    required this.isPaid,
+    required this.expiryDate,
+    this.notes = '',
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'deviceId': deviceId,
+      'fullName': fullName,
+      'isPaid': isPaid,
+      'expiryDate': expiryDate,
+      'notes': notes,
+    };
+  }
+
+  factory Subscriber.fromMap(Map<String, dynamic> map) {
+    return Subscriber(
+      deviceId: map['deviceId'] as String? ?? '',
+      fullName: map['fullName'] as String? ?? '',
+      isPaid: map['isPaid'] as bool? ?? false,
+      expiryDate: map['expiryDate'] as String? ?? '',
+      notes: map['notes'] as String? ?? '',
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Subscriber.fromJson(String source) =>
+      Subscriber.fromMap(jsonDecode(source) as Map<String, dynamic>);
+}
 
