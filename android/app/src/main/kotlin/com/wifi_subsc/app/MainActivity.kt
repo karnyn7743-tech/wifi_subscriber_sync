28d499b5a820e01e0cb47e96cc4cee87e6d342d3
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-09-24 21:47:01 +0300

    Add MainActivity for Flutter with biometrics support
    Created MainActivity class extending FlutterFragmentActivity to enable biometrics dialogs.

---
+++ android/app/src/main/kotlin/com/wifi_subsc/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.wifi_subscriber_sync
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    // ترث النشاط كـ FragmentActivity لتمكين حوارات البصمة (Biometrics Dialogs)
+}
